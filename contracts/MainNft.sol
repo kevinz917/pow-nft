42d77fdf65184d0d4fdcf7a3a65eaf33b1284e1f
Kevin Z <zkw917@gmail.com>
2021-09-15 01:11:57 -0400

    Add Proposal Test

--- contracts/MainNft.sol
+++ contracts/MainNft.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -7,54 +8,45 @@ struct WorkProof {
   string tokenURI;
 }
 
-// TODO
-// Add voting system for minting work NFTs / a review system requiring multisig
-
 contract MainNft is ERC721Enumerable, Ownable {
   mapping(uint256 => WorkProof) public workProofs; // NFT structs containing the tokenURI
   mapping(uint256 => address) public ogOwners; // Is there a way to prevent people from transfering?
-  uint256 public _nonce; // token ID
+  uint256 public tokenId; // nft token ID
 
   constructor() ERC721("Kevin's Experiment", "KZ") {
     transferOwnership(msg.sender);
   }
 
-  function mint(address recipient, string memory metadataURI) external payable {
-    // Ban self minting from admin
-    if (recipient == owner()) {
-      require(msg.sender != owner());
-    }
-    workProofs[_nonce].tokenURI = metadataURI;
-    ogOwners[_nonce] = recipient;
-    _mint(recipient, _nonce);
-    _nonce++;
+  /**
+   * @dev mint NFT
+   * @param recipient: recipient of the NFT
+   * @param metadataURI: storage url containing work info
+   */
+  function mint(address recipient, string memory metadataURI) external payable onlyOwner {
+    workProofs[tokenId].tokenURI = metadataURI;
+    ogOwners[tokenId] = recipient;
+    _mint(recipient, tokenId);
+    tokenId++;
   }
 
-  // We only allow admin to transfer the ownership of NFTs because
-  // work proofs aren't meant to be tradable!
+  /**
+   * @dev check if owner is OG
+   * @param _tokenId: nft token id
+   */
+  function isOgOwner(uint256 _tokenId) public view returns (bool) {
+    require(_tokenId <= tokenId, "Invalid token id");
+    return (msg.sender == ogOwners[_tokenId]);
+  }
+
   function transferFrom(
     address from,
     address to,
-    uint256 tokenId
+    uint256 _tokenId
   ) public virtual override onlyOwner {
-    super.transferFrom(from, to, tokenId);
+    super.transferFrom(from, to, _tokenId);
   }
 
-  function isOgOwner(uint256 tokenId) public view returns (bool) {
-    return (msg.sender == ogOwners[tokenId]);
-  }
-
-  function tokenURI(uint256 _tokenid)
-    public
-    view
-    override
-    returns (string memory)
-  {
+  function tokenURI(uint256 _tokenid) public view override returns (string memory) {
     return workProofs[_tokenid].tokenURI;
   }
-
-  // function _randomishIntLessThan(bytes32 salt, uint8 n) private view returns (uint8) {
-  //   if (n == 0) return 0;
-  //   return uint8(keccak256(abi.encodePacked(block.timestamp, _nonce, msg.sender, salt))[0]) % n;
-  // }
 }
