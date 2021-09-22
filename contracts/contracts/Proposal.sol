da100a244a979e10ebcce688809f131a4bcb571d
Kevin Z <zkw917@gmail.com>
2021-09-21 21:33:09 -0400

    Add ability to add multiple reviewers

--- contracts/contracts/Proposal.sol
+++ contracts/contracts/Proposal.sol
@@ -5,47 +5,47 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Membership.sol";
 import "./MainNft.sol";
+import "hardhat/console.sol";
+
+/**
+ * @dev creates a proposal object
+ * @param reviewers: address of the party who reviews the proposal
+ * @param nftID: count of the NFTs issued by the DAO (for internal book-keeping)
+ * @param proposalDataURI: IPFS address of the .json containing proposal details
+ * @param contributor: address of the party who submitted the proposal
+ */
+struct ProposalData {
+  uint256 nftID;
+  string proposalDataURI;
+  address contributor;
+}
 
 contract Proposal is Ownable {
-  Membership public membership;
   MainNft public NFT;
+  Membership public membership;
 
-  /**
-   * @dev initialize contract with list of proposals
-   * @param _proposalDataURIs: list of data URLs
-   */
-  constructor(string[] memory _proposalDataURIs) {
+  constructor() {
     NFT = new MainNft();
     membership = new Membership();
-
-    for (uint8 i = 0; i < _proposalDataURIs.length; i++) {
-      createProposal(_proposalDataURIs[i]);
-    }
-  }
-
-  /**
-   * @dev creates a proposal object
-   * @param reviewer: address of the party who reviews the proposal
-   * @param nftID: count of the NFTs issued by the DAO (for internal book-keeping)
-   * @param proposalDataURI: IPFS address of the .json containing proposal details
-   * @param contributor: address of the party who submitted the proposal
-   */
-  struct ProposalData {
-    address reviewer;
-    uint256 nftID;
-    string proposalDataURI;
-    address contributor;
   }
 
   uint256 public proposalID = 0; // proposal ID: counter of submitted proposals
   mapping(uint256 => ProposalData) public Proposals; // Proposals: maps proposalID to Proposals
+  mapping(uint256 => address[]) public Reviewers;
+  mapping(uint256 => mapping(address => bool)) public Approvals; // Approvals: maps proposalID to list of addresses to approval status
 
   /**
    * @dev creates a proposal object with the appropriate metadata; called by the proposer
    * @param _proposalDataURI: IPFS address of the .json file containing proposal information
+   * @param _reviewers: Array of reviewers
    */
-  function createProposal(string memory _proposalDataURI) public virtual {
-    Proposals[proposalID] = ProposalData(address(0), 0, _proposalDataURI, msg.sender);
+  function createProposal(string memory _proposalDataURI, address[] memory _reviewers) public {
+    require(_reviewers.length > 0, "At least one reviewer");
+
+    Proposals[proposalID] = ProposalData({ nftID: 0, proposalDataURI: _proposalDataURI, contributor: msg.sender });
+    for (uint256 i = 0; i < _reviewers.length; i++) {
+      Reviewers[proposalID].push(_reviewers[i]);
+    }
     proposalID++;
   }
 
@@ -53,13 +53,34 @@ contract Proposal is Ownable {
    * @dev approves a proposal and mints an NFT to the proposer's address; called by the reviewer
    * @param _proposalID: index of proposal submitted
    */
-  function approveProposal(uint256 _proposalID) public virtual onlyOwner {
+  function approveProposal(uint256 _proposalID) public virtual {
     require(_proposalID <= proposalID, "Invalid proposal");
 
-    Proposals[_proposalID].reviewer = msg.sender;
-    membership.increaseAttribute(Proposals[_proposalID].contributor);
-    NFT.mint(Proposals[_proposalID].contributor, Proposals[_proposalID].proposalDataURI);
+    // check whether user is part of the reviewers list;
+    for (uint256 i = 0; i < Reviewers[_proposalID].length; i++) {
+      if (Reviewers[_proposalID][i] == msg.sender) {
+        Approvals[_proposalID][msg.sender] = true;
+      }
+    }
+  }
 
-    Proposals[_proposalID].nftID = NFT.tokenId();
+  /**
+   * @dev triggers mint function for NFT if all approvals are done
+   * @param _proposalID: index of proposal submitted
+   */
+  function triggerMint(uint256 _proposalID) public {
+    bool allApproved = true;
+    for (uint256 i = 0; i < Reviewers[_proposalID].length; i++) {
+      address reviewer = Reviewers[_proposalID][i];
+      if (!Approvals[_proposalID][reviewer]) {
+        allApproved = false;
+      }
+    }
+
+    if (allApproved) {
+      membership.increaseAttribute(Proposals[_proposalID].contributor);
+      NFT.mint(Proposals[_proposalID].contributor, Proposals[_proposalID].proposalDataURI);
+      Proposals[_proposalID].nftID = NFT.tokenId();
+    }
   }
 }
--- contracts/test/Proposal.test.js
+++ contracts/test/Proposal.test.js
@@ -13,41 +13,48 @@ describe("Proposal", () => {
   let addr2;
 
   before(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    const reviewers = [owner.address, addr1.address, addr2.address];
+
     const proposal = await ethers.getContractFactory("Proposal");
-    proposalContract = await proposal.deploy([tokenDataUrl, tokenDataUrl]);
+    // proposalContract = await proposal.deploy([reviewers, reviewers], [tokenDataUrl, tokenDataUrl]);
+    proposalContract = await proposal.deploy();
     nftContract = await ethers.getContractAt("MainNft", await proposalContract.NFT()); // fetches contract initiated within the proposal contract
     membershipContract = await ethers.getContractAt("Membership", await proposalContract.membership());
-
-    [owner, addr1, addr2] = await ethers.getSigners();
   });
 
   it("Create proposal", async () => {
-    await proposalContract.createProposal(tokenDataUrl);
+    const reviewers = [owner.address, addr1.address, addr2.address];
+    await proposalContract.createProposal(tokenDataUrl, reviewers);
 
-    expect(await proposalContract.proposalID()).to.be.equal(3);
-
+    expect(await proposalContract.proposalID()).to.be.equal(1);
     const firstProposalData = await proposalContract.Proposals(0);
+
+    const res = await proposalContract.Reviewers(0, 0);
+    expect(res, owner.address);
+
     assert.equal(firstProposalData.proposalDataURI, tokenDataUrl);
     assert.equal(firstProposalData.nftID, 0);
-    assert.equal(firstProposalData.reviewer, emptyAddress);
     assert.equal(firstProposalData.contributor, owner.address);
   });
 
-  it("Approve proposal", async () => {
+  it("Approve proposal from one party", async () => {
     await proposalContract.approveProposal(0);
 
-    const firstProposalData = await proposalContract.Proposals(0);
-    assert.equal(firstProposalData.reviewer, owner.address);
-    assert.equal(firstProposalData.nftID, 1);
+    let approvedState = await proposalContract.Approvals(0, owner.address);
+    assert.equal(approvedState, true);
+  });
 
-    expect(await proposalContract.NFT()).to.be.equal(nftContract.address);
-    expect(await nftContract.tokenId()).to.be.equal(1);
+  it("Approve all + Mint NFT", async () => {
+    const reviewers = [owner.address, addr1.address, addr2.address];
+
+    await proposalContract.approveProposal(0);
+    await proposalContract.connect(addr1).approveProposal(0);
+    await proposalContract.connect(addr2).approveProposal(0);
+
+    await proposalContract.triggerMint(0);
 
     const character = await membershipContract.characters(owner.address);
     assert.equal(character.swag > 0 || character.strength > 0 || character.speed > 0, true);
-  });
-
-  it("Check NFT", async () => {
-    expect(await nftContract.isOgOwner(0)).to.be.equal(true);
   });
 });
